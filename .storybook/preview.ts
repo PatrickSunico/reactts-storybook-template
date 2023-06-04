23d04dde3a4862dbe12cc9d405e9488801e4d595
PatrickSunico <apatricksunico@gmail.com>
2023-06-04 13:47:53 +0300

    rollback

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,19 +1,6 @@
 import type { Preview } from "@storybook/react";
 import "../src/index.css";
 
-import { withThemeByDataAttribute } from '@storybook/addon-styling';
-
-export const decorators = [
-  withThemeByDataAttribute({
-    themes: {
-      light: 'light',
-      dark: 'dark',
-    },
-    defaultTheme: 'light',
-    attributeName: 'data-mode',
-  }),
-];
-
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
